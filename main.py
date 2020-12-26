f0a8ccb6c2fdbe51bc89f2cab1956eaaea320908
kgerasimova <74534535+kgerasimova@users.noreply.github.com>
2020-12-26 11:21:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def solution(lt=list):
         element=lt[0]
         for i in lst:
             if element>=i:
-                answer=True
+                x=True
             else:
-                answer=False
+                x=False
                 break
             element=i
         if answer:
             return answer
-        else: return  answer
+        else: return  x
